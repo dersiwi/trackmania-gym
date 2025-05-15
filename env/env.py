eb064c85af8d31ca8dca67b31fdaab3191b1d996
SgSiegens <uxgzc@student.kit.edu>
2025-05-15 12:39:52 +0200

    started writing the gymnasium environment for TMNF

---
+++ env/env.py
@@ -0,0 +1,57 @@
+import gymnasium as gym
+from gymnasium.spaces import Discrete, Box
+import numpy as np
+
+class TMNF_Env(gym.Env):
+    """The reinforcement learning environment for Trackmania Nations Forever"""
+
+    # init, step and reset have to be implemented for the class to be gym compatible
+
+    def __init__(self):
+        """
+        Initializes the custom Gymnasium environment.
+        This constructor sets up the basic structure of the environment.
+        As required by Gymnasium environments, it defines the action and observation spaces.
+        We also define some other important varibales in order to communicate with TMInterface
+        """
+        super().__init__()
+
+    def step(self, action):
+        """
+        The `step` function is a core component of the Gymnasium environment API and contains 
+        the main game logic, including state transitions and reward calculations.
+        This method applies the specified action to the environment, updates the 
+        internal state accordingly, and returns the results of that action.
+
+        Parameters:
+            action (TODO)
+
+        Returns:
+        observation (TODO): The initial observation of the environment.
+        reward (float): The scalar reward signal for the agent's action.
+        terminated (bool): Whether the episode has ended because the task is successfully 
+                           completed (e.g., agent reached the goal).
+        truncated (bool): Whether the episode ended due to a time limit or other external cutoff.
+        info (dict): A dictionary with additional information (e.g. for debugging or logging).
+        """
+        return super().step(action)
+    
+    def reset(self, *, seed = None, options = None):
+        """
+        Resets the environment to start a new episode.
+
+        Parameters:
+            seed (int, optional): Seed for the random number generator to ensure 
+                              deterministic behavior. Used by calling `super().reset(seed=seed)`.
+            options (dict, optional): Additional options for resetting the environment. 
+                                    These can be used to modify behavior at reset time.
+
+        Returns:
+            observation (TODO): The initial observation of the environment.
+            info (dict): Additional information that may be useful for debugging or analysis.
+    
+        This method is required by the Gymnasium API and is called at the beginning of each new episode.
+        It ensures the environment starts in a consistent and valid state.
+        """
+        return super().reset(seed=seed, options=options)
+    
