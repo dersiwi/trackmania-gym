4a78515a4549dfba3b58582ed0acaed8db0a3186
Simon Wilhelm Schübel <siwischbl@gmail.com>
2025-05-23 12:10:15 +0200

    Added inux flag to example.py

--- scripts/example.py
+++ scripts/example.py
@@ -151,13 +151,21 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--tmi_port", "-p", type=int, default=8775)
     parser.add_argument("--launch", "-l", action="store_true",  default=False)
+    parser.add_argument("--linux", "-u", action="store_true",  default=False)
     parser.add_argument("--reqimgs", "-imgs", action="store_true",  help="If set, requests images each simulation step and stores them in the current directory in a /frame folder. [WARNING : This is a ton of frames, even for short amounts of running it.]", default=False)
 
     args = parser.parse_args()
 
-    GMI = GameInstanceManager.get_instance(TMLoader_path = Path(os.path.expanduser("~")) / "AppData" / "Local" / "TMLoader" / "TMLoader.exe",
-                            path_to_plugin = Path(os.path.expanduser("~")) / "OneDrive" / "Dokumente" / "TMInterface" /"Plugins" / "Python_Link.as",
-                            linux = False)
+    if args.linux:
+        tmloader = None
+        plugin = None
+    else:
+        tmloader = Path(os.path.expanduser("~")) / "AppData" / "Local" / "TMLoader" / "TMLoader.exe"
+        plugin = Path(os.path.expanduser("~")) / "OneDrive" / "Dokumente" / "TMInterface" /"Plugins" / "Python_Link.as"
+
+    GMI = GameInstanceManager.get_instance(TMLoader_path = tmloader,
+                            path_to_plugin = plugin,
+                            linux = args.linux)
     
     if args.launch:
         GMI.launch_game()
