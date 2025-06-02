3c455f80565aad0ce7aef28d3e8a788ee8dce5eb
SgSiegens <uxgzc@student.kit.edu>
2025-06-02 23:15:37 +0200

    - updated gitignore - added wandb to train.yaml - extractor works now on batches

--- neuronal_networks/custom_extractor.py
+++ neuronal_networks/custom_extractor.py
@@ -34,46 +34,64 @@ class TMN_Extractor(BaseFeaturesExtractor):
     ) -> None:
         super().__init__(observation_space, features_dim=1)
 
-        extractors: dict[str, nn.Module] = {}
-        #  pytroch ModuleDict cant handel dots in key so we remove the dots but later we must know what the orginal key was 
-        self.key_mappings: dict[str,str] = {}
         total_concat_size = 0
+        extractors: dict[str, nn.Module] = {}
+
+        """
+        pytorch ModuleDict cant handel dots in key so we remove 
+        the dots but later we must know what the orginal key was 
+        """  
+        self.key_mappings: dict[str,str] = {}
+
         for key, subspace in observation_space.spaces.items():
+
             if key == "image":
                 extractors[key] = vision_model
                 total_concat_size += vision_model_out_dim
                 self.key_mappings[key] = key
             else:
-                """
-                TODO do we really need to project down ? 
-                Should our final feature vector be not that long ? -> could leed to not good training  
-                see https://bmild.github.io/fourfeat/index.html
-                """
-                # pytroch ModuleDict cant handle dots in key 
+                # remove the dots for the pytorch ModuleDict but remember the actual key
                 new_key = key.replace('.',"")
                 self.key_mappings[new_key] = key
-                if len(subspace.shape) == 1 and subspace.shape[0] > 50 :
-                    extractors[new_key] = nn.Linear(subspace.shape[0], subspace.shape[0] //4)
-                    total_concat_size += subspace.shape[0] //4
 
-                elif len(subspace.shape) <= 1:
-                    extractors[new_key] = nn.Identity()
+                # if subspace is a vector whith length > thresh (here 50) then project into lower dimension
+                if len(subspace.shape) == 1:
+                    if subspace.shape[0] > 50 :
+                        extractors[new_key] = nn.Linear(subspace.shape[0], subspace.shape[0] //4)
+                        total_concat_size += subspace.shape[0] //4
+                    else: 
+                        extractors[new_key] = nn.Identity() # ensure that batch dim gets added 
+                        total_concat_size += gym.spaces.utils.flatdim(subspace)
+
+                # scalars and not so long vectors get passed through
+                elif len(subspace.shape) < 1:
+                    extractors[new_key] = Add_Batch() # ensure that batch dim gets added 
                     total_concat_size += gym.spaces.utils.flatdim(subspace)
 
+                # this is for the rotation_marix since it is the only field which has 2d shape but is not an image
                 else:
-                    extractors[new_key] = nn.Flatten(start_dim=0)
+                    extractors[new_key] = nn.Flatten(start_dim=1)
                     total_concat_size += gym.spaces.utils.flatdim(subspace)
-
-        self.extractors = nn.ModuleDict(extractors)
 
         # Update the features dim manually
         self._features_dim = total_concat_size
+        self.extractors = nn.ModuleDict(extractors)
 
     def forward(self, observations: dict[str, torch.Tensor]) -> torch.Tensor:
         encoded_tensor_list = []
         for key, extractor in self.extractors.items():
             tensor = extractor(observations[self.key_mappings[key]])
-            if key == "image" : tensor = tensor[0]
-            if len(tensor.shape) == 2 : tensor = torch.squeeze(tensor)
             encoded_tensor_list.append(tensor)
-        return torch.cat(encoded_tensor_list, dim=-1)
+         # Return a (B, self._features_dim) PyTorch tensor, where B is batch dimension.
+        return torch.cat(encoded_tensor_list, dim=1)
+
+
+# TODO as for now this works but is not pretty. Thinking about refactoring it
+class Add_Batch(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self,x):
+        if len(x.shape) == 0: return x.unsqueeze(0).unsqueeze(0)
+        if len(x.shape) ==1 : return x.unsqueeze(0)
+        return x
--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,7 @@
 # utils
 import sys, os
+
+import omegaconf
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # TODO : <- i don't want this here and it shouldnt have to be here!!!
 from utils.scriptargs import get_argparser
 from pathlib import Path
@@ -38,6 +40,9 @@ from stable_baselines3.common.policies import ActorCriticPolicy
 import hydra
 from omegaconf import DictConfig,OmegaConf
 
+import wandb
+from wandb.integration.sb3 import WandbCallback
+
 _HYDRA_PARAMS = {
     "version_base": "1.3",
     "config_path": "../configs",
@@ -46,10 +51,21 @@ _HYDRA_PARAMS = {
 
 @hydra.main(**_HYDRA_PARAMS)
 def main(cfg:DictConfig):
+    if True:
+        wandb.login()
+        wandb.config = omegaconf.OmegaConf.to_container(
+        cfg, resolve=True, throw_on_missing=True
+        )
+        run = wandb.init(entity=cfg.wandb.entity, 
+               project=cfg.wandb.project,
+                sync_tensorboard=True, 
+                monitor_gym=True,  
+                save_code=True)
+    
     platform = cfg.platforms
-    args = get_argparser()
-    config_logging()
-    logger = logging.getLogger("examplescript")
+    #args = get_argparser()
+    #config_logging()
+    #logger = logging.getLogger("examplescript")
 
     IMG_WIDTH, IMG_HEIHGT = cfg.image.width, cfg.image.height
 
@@ -73,10 +89,10 @@ def main(cfg:DictConfig):
     startsignal = response_queue.get(timeout = 60)
     assert startsignal["cmd_id"] == 512 and startsignal["status"] == 0
 
+    #gym.make_vec("TMNF_Single_Agent_ENV_v0",num_envs=2,)
     tm_env = TMNF_Single_Agent_Env(
         command_queue=control_queue,
         response_queue=response_queue)
-    
     #observations_list= cfg.rl_env.observations_list
     #observations_list = list(simstate_space_dict.keys())
     for _,wrapper_conf in cfg.rl_env.wrappers.items():
@@ -91,19 +107,18 @@ def main(cfg:DictConfig):
     vision_model = vision_model,
     vision_model_out_dim = vision_model.out_dims))
 
-    model = hydra.utils.instantiate(cfg.sb3)(env= tm_env, policy_kwargs=policy_kwargs)
+    model = hydra.utils.instantiate(cfg.sb3)(env= tm_env, policy_kwargs=policy_kwargs,tensorboard_log= f"runs/{run.id}",n_steps=10)
+    print("Number of learnable params: ",len(list(model.policy.named_parameters())))
     #model = PPO("MultiInputPolicy", env= tm_env, policy_kwargs=policy_kwargs, verbose=1)
-    for name, param in model.policy.named_parameters():
-        if param.requires_grad:
-            print(f"{name}: {param.shape}")
             
-    model.learn(cfg.total_timesteps)
+    model.learn(total_timesteps =cfg.total_timesteps,progress_bar=True,
+                callback=WandbCallback(gradient_save_freq=100,model_save_path=f"models/{run.id}",verbose=2,),)
 
     control_queue.put(TMIProcessWrapper.IPCCommands.get_end_syncloop_command(1000)) #1000 doesnt matter.
     p.join()
         
-    if args.launch:
-        GIM.close_game()
+   
+    GIM.close_game()
 
 
 if __name__ == "__main__": 
