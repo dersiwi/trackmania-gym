84627bba512b5fb9aca7ebb607dce9c9dc127816
Simon Wilhelm Schübel <siwischbl@gmail.com>
2025-06-13 20:27:34 +0200

    Moved lineisght observation configuration initialization into utility method.

--- scripts/train.py
+++ scripts/train.py
@@ -25,15 +25,10 @@ from stable_baselines3.common.base_class import BaseAlgorithm
 from trackmania_env.envs.single_agent_env2 import TMNF_Single_Agent_Env
 from trackmania_env.observations.observation_manager import ObservationManager
 from trackmania_env.observations.linesight_obs_wrapper import LinesightObservationWrapper
-from trackmania_env.utils.map_loader import (
-    load_map_with_extrapolated_centers,
-    precalculate_virtual_checkpoints_information,
-    sync_virtual_and_real_checkpoints)
-
 
 from configs.config import TrainConfig
 from utils.printutils import print_model_params
-
+from trackmania_env.utils.init_linesight_obs import get_linesight_obs_instance
 
 _HYDRA_PARAMS = {
     "version_base": "1.3",
@@ -74,43 +69,8 @@ def main(cfg : TrainConfig):
                                             img_width=cfg.image.width, 
                                             img_height=cfg.image.height)
         elif cfg.rl_env.env.obs_manager =="linesight":
-            linesightcfg = cfg.rl_env.linesightobsmanager
-            zone_centers = load_map_with_extrapolated_centers(
-                n_before = linesightcfg.n_zone_centers_extrapolate_before_start_of_map,
-                n_after  = linesightcfg.n_zone_centers_extrapolate_after_end_of_map,
-                path_to_file = linesightcfg.reference_line,)
+            obs_manager = get_linesight_obs_instance(cfg)
             
-            (
-                zone_transitions,
-                distance_between_zone_transitions,
-                distance_from_start_track_to_prev_zone_transition,
-                normalized_vector_along_track_axis,
-            ) = precalculate_virtual_checkpoints_information(zone_centers)
-
-            (
-                next_real_checkpoint_positions,
-                max_allowable_distance_to_real_checkpoint,
-            ) = sync_virtual_and_real_checkpoints(
-                zone_centers, 
-                cfg.platforms.map_dir+'/'+cfg.gmi.track,
-                linesightcfg.sync_virtual_and_real_checkpoints)
-
-
-            obs_manager = LinesightObservationWrapper(observation_list=obs_manager_cfg.observation_list, 
-                                            colorspace=obs_manager_cfg.colorspace,
-                                            convert_torch=obs_manager_cfg.convert_torch,
-                                            img_width=cfg.image.width, 
-                                            img_height=cfg.image.height,
-
-                                            zone_centers = zone_centers,
-                                            zone_transitions = zone_transitions,
-                                            distance_between_zone_transitions = distance_between_zone_transitions,
-                                            distance_from_start_track_to_prev_zone_transition = distance_from_start_track_to_prev_zone_transition,
-                                            normalized_vector_along_track_axis = normalized_vector_along_track_axis,
-                                            next_real_checkpoint_positions = next_real_checkpoint_positions,
-                                            max_allowable_distance_to_real_checkpoint = max_allowable_distance_to_real_checkpoint,
-
-                                            cfg = cfg.rl_env.linesightobsmanager)
             
         tm_env = TMNF_Single_Agent_Env(command_queue=control_queue,
                                         response_queue=response_queue, 
---
+++ trackmania_env/utils/init_linesight_obs.py
@@ -0,0 +1,51 @@
+
+from trackmania_env.observations.linesight_obs_wrapper import LinesightObservationWrapper
+from configs.config import LinesightObsCfg, ObservationManager, ImageConfig, RLEnvConfig, TrainConfig
+
+from trackmania_env.utils.map_loader import (
+    load_map_with_extrapolated_centers,
+    precalculate_virtual_checkpoints_information,
+    sync_virtual_and_real_checkpoints)
+
+
+
+def get_linesight_obs_instance(cfg : TrainConfig):
+    linesightcfg : LinesightObsCfg = cfg.rl_env.linesightobsmanager
+    obsconfig : ObservationManager = cfg.rl_env.obsmanager
+    image_cfg : ImageConfig = cfg.image
+    zone_centers = load_map_with_extrapolated_centers(
+        n_before = linesightcfg.n_zone_centers_extrapolate_before_start_of_map,
+        n_after  = linesightcfg.n_zone_centers_extrapolate_after_end_of_map,
+        path_to_file = linesightcfg.reference_line,)
+
+    (
+        zone_transitions,
+        distance_between_zone_transitions,
+        distance_from_start_track_to_prev_zone_transition,
+        normalized_vector_along_track_axis,
+    ) = precalculate_virtual_checkpoints_information(zone_centers)
+
+    (
+        next_real_checkpoint_positions,
+        max_allowable_distance_to_real_checkpoint,
+    ) = sync_virtual_and_real_checkpoints(
+        zone_centers, 
+        cfg.platforms.map_dir+'/'+cfg.gmi.track,
+        linesightcfg.sync_virtual_and_real_checkpoints)
+
+
+    return LinesightObservationWrapper(observation_list=obsconfig.observation_list, 
+                                    colorspace=obsconfig.colorspace,
+                                    convert_torch=obsconfig.convert_torch,
+                                    img_width=image_cfg.width, 
+                                    img_height=image_cfg.height,
+
+                                    zone_centers = zone_centers,
+                                    zone_transitions = zone_transitions,
+                                    distance_between_zone_transitions = distance_between_zone_transitions,
+                                    distance_from_start_track_to_prev_zone_transition = distance_from_start_track_to_prev_zone_transition,
+                                    normalized_vector_along_track_axis = normalized_vector_along_track_axis,
+                                    next_real_checkpoint_positions = next_real_checkpoint_positions,
+                                    max_allowable_distance_to_real_checkpoint = max_allowable_distance_to_real_checkpoint,
+
+                                    cfg = cfg.rl_env.linesightobsmanager)
