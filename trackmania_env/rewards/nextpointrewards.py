eb388c580342486fe314df87efd74c53095822f3
Simon Wilhelm Schübel <siwischbl@gmail.com>
2025-06-16 16:31:48 +0200

    Added simplevalidated and velocity reward.

--- trackmania_env/rewards/nextpointrewards.py
+++ trackmania_env/rewards/nextpointrewards.py
@@ -20,6 +20,7 @@ class NextPointRewards(RewradCalculator):
         self.race_not_finished_weight = 0.05
         self.race_finished_reward = 100
         self.other_termination_punishment = 2
+        self.velocity_reward_weight = 1
 
         self.logger = logging.getLogger(self.__class__.__name__)
         self.logging_freq = 1000
@@ -31,15 +32,15 @@ class NextPointRewards(RewradCalculator):
         reward = 0
         
         i, d, drel = self.refline_manager.get_distance_to_next_point(ssD.position)
-
+        velocity_reward = self.velocity_reward_weight * np.linalg.norm(np.array(ssD.velocity) / 1000) #1000 == max velocity
         accum_dist_reward = self.accum_distance_weight * drel
         race_not_finished_reward = (-1) * self.race_not_finished_weight
         race_finished = race_finished * self.race_finished_reward
         other_term_reward = (-1) * other_terminations * self.other_termination_punishment
         
-        reward = accum_dist_reward + race_not_finished_reward + race_finished + other_term_reward
+        reward = accum_dist_reward + race_not_finished_reward + race_finished + other_term_reward + velocity_reward
         if self.step % self.logging_freq == 0:
-            self.logger.info(f"Calculated_Rewards;{reward}:accumulated_distance;{accum_dist_reward};{i};{d}:race_not_finished;{race_not_finished_reward}:race_finished;{race_finished}:other_terminations;{other_term_reward}")
+            self.logger.info(f"Calculated_Rewards;{reward}:accumulated_distance;{accum_dist_reward};{i};{d}:race_not_finished;{race_not_finished_reward}:race_finished;{race_finished}:other_terminations;{other_term_reward}:vel_reward;{velocity_reward}")
         self.step += 1
         return reward
 
