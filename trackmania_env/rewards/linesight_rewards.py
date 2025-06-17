2e5c27d78e3711de984207e946bd1692c80b5c9d
Simon Wilhelm Schübel <siwischbl@gmail.com>
2025-06-17 15:15:17 +0200

    Added linesight-reward terms to infodict and and added negative distance reward in next-rewards.

--- trackmania_env/rewards/linesight_rewards.py
+++ trackmania_env/rewards/linesight_rewards.py
@@ -35,47 +35,55 @@ class LinesightRewardCalculator(RewradCalculator):
         engineered_reward_min_dist_to_cur_vcp = 5
         engineered_reward_max_dist_to_cur_vcp = 25
 
-        reward = 0
-        
-        reward += constant_reward_per_ms 
+
         """* (
             ms_per_action
             if (i < n_frames - 1 or ("race_time" not in rollout_results))
             else rollout_results["race_time"] - (n_frames - 2) * config_copy.ms_per_action
         )"""
-        reward += (
-            observations["meters_advanced_along_centerline"] - self.last_obs["meters_advanced_along_centerline"]
-        ) * reward_per_m_advanced_along_centerline
+        meters_advanced_along_centerline_rew = (observations["meters_advanced_along_centerline"] - self.last_obs["meters_advanced_along_centerline"]) * reward_per_m_advanced_along_centerline
         
         
         v_x, v_z = ssD.velocity[1], ssD.velocity[2] # v_x is actually index 1
+        velocity_change_reward = 0
         if not self.last_obs == None:
             
             if final_speed_reward_per_m_per_s != 0 and v_z > 0:
                 # car has velocity *forward*
-                reward += final_speed_reward_per_m_per_s * (np.linalg.norm(ssD.velocity) - np.linalg.norm(self.last_obs.velocity))
+                velocity_change_reward = final_speed_reward_per_m_per_s * (np.linalg.norm(ssD.velocity) - np.linalg.norm(self.last_obs.velocity))
 
 
 
-
+        speedslide_reward = 0
         if engineered_speedslide_reward != 0 and all(*(ws.has_ground_contact for ws in wheel_state)):
             # all wheels touch the ground
-            reward += engineered_speedslide_reward * max(0.0, 1 - abs(speedslide_quality_tarmac(v_x, v_z) - 1))
+            speedslide_reward = engineered_speedslide_reward * max(0.0, 1 - abs(speedslide_quality_tarmac(v_x, v_z) - 1))
 
         # lateral speed is higher than 2 meters per second
-        reward += engineered_neoslide_reward if abs(v_x) >= 2.0 else 0
+        lateral_speed_reward = engineered_neoslide_reward if abs(v_x) >= 2.0 else 0
 
 
         # kamikaze reward
+        kamikaze_reward = 0
         if ( any(*(ws.has_ground_contact for ws in wheel_state))): #TODO : # engineered_kamikaze_reward != 0 and rollout_results["actions"][i] <= 2 or
-            reward += engineered_kamikaze_reward
+            kamikaze_reward = engineered_kamikaze_reward
 
-
+        too_close_to_vcp = 0
         if engineered_close_to_vcp_reward != 0:
-            reward += engineered_close_to_vcp_reward * max(
+            too_close_to_vcp = engineered_close_to_vcp_reward * max(
                 engineered_reward_min_dist_to_cur_vcp,
                 min(engineered_reward_max_dist_to_cur_vcp, np.linalg.norm(observations["state_zone_center_coordinates_in_car_reference_system"])),
             )
-        return reward, {}
+        reward = constant_reward_per_ms + meters_advanced_along_centerline_rew 
+        reward += velocity_change_reward + kamikaze_reward + lateral_speed_reward + too_close_to_vcp
+        reward += speedslide_reward
+        
+        return reward, {"const_rew_per_ms" : constant_reward_per_ms,
+                        "meters_advanced_along_centerline_rew" : meters_advanced_along_centerline_rew,
+                        "velocity_change_reward" : velocity_change_reward,
+                        "kamikaze_reward" : kamikaze_reward,
+                        "lateral_speed_reward" : lateral_speed_reward,
+                        "too_close_to_vcp" : too_close_to_vcp,
+                        "speedslide_reward" : speedslide_reward}
 
 
--- trackmania_env/rewards/nextpointrewards.py
+++ trackmania_env/rewards/nextpointrewards.py
@@ -20,40 +20,46 @@ class NextPointRewards(RewradCalculator):
         self.race_not_finished_weight = 0.05
         self.race_finished_reward = 100
         self.other_termination_punishment = 2
-        self.velocity_reward_weight = 1
+        self.velocity_reward_weight = 2
+        self.backward_weight = 5
 
         self.logger = logging.getLogger(self.__class__.__name__)
         self.logging_freq = 1000
-        self.step = 0
+        
+        self.current_refline_idx : int = 0
 
 
     def calculate_reward(self, observations, race_finished, other_terminations):
         ssD : SimStateData = observations[IPCFields.SIMSTATE]
         reward = 0
         
-        i, d, drel = self.refline_manager.get_distance_to_next_point(ssD.position)
+        next_refline_index, d, drel = self.refline_manager.get_distance_to_next_point(ssD.position)
+        accum_dist_reward = 0
+        if not self.current_refline_idx == next_refline_index: #only give reward if progress in regards to last one was made
+            accum_dist_reward = self.accum_distance_weight * drel
+            self.current_refline_idx = next_refline_index
+        
         velocity_reward = self.velocity_reward_weight * np.linalg.norm(np.array(ssD.velocity) / 1000) #1000 == max velocity
-        accum_dist_reward = self.accum_distance_weight * drel
         race_not_finished_reward = (-1) * self.race_not_finished_weight
         race_finished = race_finished * self.race_finished_reward
         other_term_reward = (-1) * other_terminations * self.other_termination_punishment
+        backward_punishment = (-1) * np.clip(d, min=0, max=100) / 100 * self.backward_weight
         
-        reward = accum_dist_reward + race_not_finished_reward + race_finished + other_term_reward + velocity_reward
-        if self.step % self.logging_freq == 0:
-            self.logger.info(f"Calculated_Rewards;{reward}:accumulated_distance;{accum_dist_reward};{i};{d}:race_not_finished;{race_not_finished_reward}:race_finished;{race_finished}:other_terminations;{other_term_reward}:vel_reward;{velocity_reward}")
-        self.step += 1
+        reward = accum_dist_reward + race_not_finished_reward + race_finished + other_term_reward + velocity_reward + backward_punishment
+
         return reward, {"total" : reward, 
                         "accumulated_distance" : accum_dist_reward,
-                        "nextpoint_reference_index" : i,
+                        "nextpoint_reference_index" : next_refline_index,
                         "race_not_finished":race_not_finished_reward,
                         "race_finished" : race_finished,
                         "other_terminations":other_term_reward,
-                        "vel_reward":velocity_reward}
+                        "vel_reward":velocity_reward,
+                        "backward_punishment" : backward_punishment}
 
 
 
 
     def reset(self):
         self.refline_manager.reset()
-        self.step = 0
+        self.current_refline_idx
         return super().reset()
