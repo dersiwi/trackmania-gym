Implemented Timeout-Policy for Environment, moved timeout to trucated instead of terminated, expanded reward-calculation-input to processed obs (TODO : have to fix lineisght rewards). Resolves #37.

trackmania_env/rewards/basic_rewards.py
@@ -10,7 +10,7 @@ class BasicRewardCalculation(RewradCalculator):
     def __init__(self, position_buffer):
         super().__init__(position_buffer)
 
-    def calculate_reward(self, observations, race_finished, other_terminations) -> tuple[float, dict[str, any]]:
+    def calculate_reward(self, observations, processed_obs : dict[str, any], race_finished, other_terminations) -> tuple[float, dict[str, any]]:
         ssD : SimStateData = observations[IPCFields.SIMSTATE]
         velocity = ssD.velocity
         reward = np.linalg.norm(velocity[0:1]) + self.pos_buffer.distance_moved()