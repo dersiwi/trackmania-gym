c6dc3393b4103329ef1b0244fbb7afef7d48d09f
Simon Wilhelm Schübel <siwischbl@gmail.com>
2025-06-19 18:07:06 +0200

    'Fixed' example_extractor.py with current config-setup

--- scripts/examples/example_extractor.py
+++ scripts/examples/example_extractor.py
@@ -1,106 +1,82 @@
-# utils
 import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # TODO : <- i don't want this here and it shouldnt have to be here!!!
-from utils.scriptargs import get_argparser
 from pathlib import Path
-from contextlib import redirect_stdout
-from multiprocessing import Queue, Process
-
-# imports for communication between TMInterface and environment
-from trackmania_env.envs.single_agent_env2 import TMNF_Single_Agent_Env
-from game_interaction.game_instance_manager2 import GameInstanceManager
-from game_interaction.run_multiprocess_wrapper import run_wrapper
-from game_interaction.process_wrapper import TMIProcessWrapper
-
-# gymnasium environment wrapper 
-from trackmania_env.wrappers.observations_filter import ObservationFilter
-from trackmania_env.wrappers.bgra_to_rgb import BGRA_to_RGB
-from trackmania_env.wrappers.transform_grayscale import TransformGrayscale
-from trackmania_env.wrappers.transform_torch import PytorchWrapper
-
-# extractor imports
-from neuronal_networks.conv_NNs import PrebuiltResNet
-from neuronal_networks.custom_extractor import TMN_Extractor
-
-from simstate_space_dict import simstate_space_dict
-
 import gymnasium as gym
+from contextlib import redirect_stdout
 import numpy as np
+from multiprocessing import Queue, Process
+import time 
 import logging
 
 
-from utils.scriptargs import get_argparser, get_paths, config_logging
-
-if __name__ == "__main__":
-    args = get_argparser()
-    TMLoader_path, path_to_plugin = get_paths(args.linux)
-    config_logging()
-    logger = logging.getLogger("examplescript")
+sys.path.append(os.path.abspath(os.path.join(
+    os.path.join(os.path.dirname(__file__), '..'), '..'))) # TODO : <- i don't want this here and it shouldnt have to be here!!!
 
 
-    IMG_WIDTH, IMG_HEIHGT = 180, 160
+from trackmania_env.envs.single_agent_env2 import TMNF_Single_Agent_Env
+from trackmania_env.envs.testenv_single_agent import TestEnvironment, TestLinesightRewards, PrintRewardsToConsole
 
-    GIM = GameInstanceManager.get_instance(
-        TMLoader_path = TMLoader_path,
-        path_to_plugin = path_to_plugin,
-        linux = args.linux,
-        headless= False)
-
-    # instanciate a SyncManager.
-    control_queue = Queue() # queue for commands to send to TMIProcessWrapper
-    response_queue = Queue() # answers (payload) from TMIProcess Wrapper
-   
-
-    p = Process(target=run_wrapper, args=(GIM, args.launch, control_queue, response_queue, IMG_WIDTH, IMG_HEIHGT))
-    
-    p.start()
+from trackmania_env.utils.actionmap import get_reverse_action_map
 
 
-    # wait for trackmania to load map and start simulation.
-    control_queue.put_nowait(TMIProcessWrapper.IPCCommands.get_startsignal(512))
-    startsignal = response_queue.get(timeout = 60)
-    assert startsignal["cmd_id"] == 512 and startsignal["status"] == 0
 
-    tm_env = TMNF_Single_Agent_Env(
+from game_interaction.process_wrapper import TMIProcessWrapper
+
+
+from game_interaction.run_multiprocess_wrapper import start_process_and_wait_for_startsignal
+from trackmania_env.observations.observations import get_observation_manager
+from trackmania_env.rewards.getrewards import get_reward_calculator
+
+from neuronal_networks.conv_NNs import PrebuiltResNet
+from neuronal_networks.custom_extractor import TMN_Extractor
+
+import hydra
+from configs.config import TrainConfig
+
+_HYDRA_PARAMS = {
+    "version_base": "1.3",
+    "config_path": "../../configs",
+    "config_name": "train.yaml",
+}
+
+@hydra.main(**_HYDRA_PARAMS)
+def main(cfg : TrainConfig):
+
+    tmi_process, control_queue, response_queue = start_process_and_wait_for_startsignal(cfg.platforms, cfg.gmi, cfg.image.width, cfg.image.height)
+
+    tm_env = TestEnvironment(
         command_queue=control_queue,
-        response_queue=response_queue)
+        response_queue=response_queue,
+        obs_manager=get_observation_manager(cfg), 
+        reward_calculator=get_reward_calculator(cfg),
+        env_cfg=cfg.rl_env.env,
+        platform=cfg.platforms.os)
     
-    observations_list= [
-            "image",
-            "position",
-            "velocity",
-            "yaw_pitch_roll",
-            "scene_mobil.sync_vehicle_state.speed_forward",
-            "scene_mobil.input_steer",
-            "scene_mobil.async_vehicle_state.is_turbo",
-            "scene_mobil.engine.gear",
-            "input_finish_event.input_data_field",
-            "input_brake_event.last_field",
-            "input_accelerate_event.time_field",
-            "scene_mobil.async_vehicle_state.rest_field",
-            "player_info.last_field",
-            ]
-    #observations_list = list(simstate_space_dict.keys())
-    tm_env = ObservationFilter(tm_env,observations_list)
-    tm_env = BGRA_to_RGB(tm_env)
-    tm_env = TransformGrayscale(tm_env,keep_dim=True)
-    tm_env = PytorchWrapper(tm_env)
+    tm_env.add_env_test_calback(TestLinesightRewards())
+    tm_env.add_env_test_calback(PrintRewardsToConsole())
+    
+    tm_env.step_with_manual_input()
+    
+    control_queue.put(TMIProcessWrapper.IPCCommands.get_end_syncloop_command(1000)) #1000 doesnt matter.
+    tmi_process.join()
 
     out_dim = 10
     vision_model = PrebuiltResNet("resnet18",out_dims=out_dim,pretrained=True,
-                                  in_color_channels=tm_env.observation_space["image"].shape[-1])
+                                    in_color_channels=tm_env.observation_space["image"].shape[-1])
     extractor = TMN_Extractor(tm_env.observation_space,vision_model=vision_model,vision_model_out_dim=out_dim)
     with open('example_extractor_out.txt', 'w') as f:
         with redirect_stdout(f):
-             for i in range(20):
+            for i in range(20):
                 obs,_,_,_,_ = tm_env.step(np.random.randint(0,12))
                 v = extractor(obs)
                 print(v)
                 print(v.shape)
                 print("-"*20)
-   
-    control_queue.put(TMIProcessWrapper.IPCCommands.get_end_syncloop_command(1000)) #1000 doesnt matter.
-    p.join()
-        
-    if args.launch:
-        GIM.close_game()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
