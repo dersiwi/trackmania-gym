Fixed important bugs in reward calculation.

trackmania_env/rewards/nextpointrewards.py
@@ -54,18 +54,21 @@ class NextPointRewards(RewradCalculator):
         if not self.last_simstate == None:
             velocity_change_reward = (current_velocity_normed - self._get_normed_velocity(self.last_simstate.velocity)) * self.velocity_change_reward_weight
 
-        
-        distance_to_center_reward = self.distance_to_center_weight * (1 - np.clip(self.refline_manager.calculate_lateral_difference(idx = next_refline_index, car_position=ssD.position), 
-                                                a_min = 0, a_max = self.max_lateral_difference) / self.max_lateral_difference) # inverse the distance, such that reward is bigger once distance gets smaller
-        
-        velocity_reward = self.velocity_reward_weight * current_velocity_normed
+        distance_to_center_reward = 0
+        if next_refline_index > 0:
+            # only calculate reward to centerline once car is within the firsrt linesgement, 
+            # e.g. if the agent drives backwards out of map immediately he will always get max reward, because this term will always be 1/12 * self.ditsance_to_center_weight
+            distance_to_center_reward = self.distance_to_center_weight * (1 - np.clip(self.refline_manager.calculate_lateral_difference(idx = next_refline_index, car_position=ssD.position), 
+                                                    a_min = 0, a_max = self.max_lateral_difference) / self.max_lateral_difference) # inverse the distance, such that reward is bigger once distance gets smaller
+            
+        #velocity_reward = self.velocity_reward_weight * current_velocity_normed
         race_not_finished_reward = (-1) * self.race_not_finished_weight
         race_finished = race_finished * self.race_finished_reward
         other_term_reward = (-1) * other_terminations * self.other_termination_punishment
         backward_punishment = (-1) * np.clip(d, a_min=0, a_max=100) / 100 * self.backward_weight
         
-        reward = accum_dist_reward + race_not_finished_reward + race_finished + other_term_reward + velocity_reward + backward_punishment + distance_to_center_reward + velocity_change_reward
-        self.last_observations = ssD
+        reward = accum_dist_reward + race_not_finished_reward + race_finished + other_term_reward + backward_punishment + distance_to_center_reward + velocity_change_reward
+        self.last_simstate = ssD
         return reward, {"total" : reward, 
                         "accumulated_distance" : accum_dist_reward,
                         "distance_to_center" : distance_to_center_reward,
@@ -73,7 +76,6 @@ class NextPointRewards(RewradCalculator):
                         "race_not_finished":race_not_finished_reward,
                         "race_finished" : race_finished,
                         "other_terminations":other_term_reward,
-                        "vel_reward":velocity_reward,
                         "backward_punishment" : backward_punishment,
                         "velocity_change_reward" : velocity_change_reward}
 