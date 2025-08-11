41400daf9fb90da95ca709e9345da4d56507bdb2
Simon Wilhelm Schübel <siwischbl@gmail.com>
2025-08-12 00:30:13 +0200

    Added docstirngs to sec_env. And also some truncated=True, in case of crash on step-method.

--- trackmania_env/envs/sec_env.py
+++ trackmania_env/envs/sec_env.py
@@ -20,9 +20,24 @@ from game_interaction.ipc_fields import IPCCommands
 
 
 class CrashProofEnvironment(gym.Env):
+    """
+    This class implements a wrapper for TMNF_Single_Agent_Env. During training and testing we experienced weird behaviour
+    of the process wrapper; after a seemingly random prevent-sim-finish the process wrapper would not answer the commands of 
+    the environment anymore. Even sending the command multiple times did not recover the state.
+
+    This class is able to detect the error and if it catches the error, it restarts the enviornment and game completely. This enables the
+    learner process using this class to have a clearn interface.
+
+    If the error is detected, no further informaiton from the current enviornment can be gathered. Therefore, the last recorded state is sent back
+    once more; however truncated is set to True. The learner process can then call reset, in order to keep disruption to trajectory collection mininmal.   
+    """
 
 
     def __init__(self, train_cfg : TrainConfig):
+        """
+        Args:
+            train_cfg (TrainConfig) : Configuration file used for initialization of enviroment  
+        """
         super().__init__()
 
         self.cfg : TrainConfig = train_cfg
@@ -34,6 +49,10 @@ class CrashProofEnvironment(gym.Env):
 
         self.env_initalizations : int = 0
         self.queue_empty_error  : int = 0
+
+        self.total_timesteps = 0
+        self._step_recorded_at_timestep = -1
+        self._last_obs, self._last_rew, self._last_terminated, self._last_truncated, self._last_info = None, None, None, None, None
         self.logger = logging.getLogger(self.__class__.__name__)
         
 
@@ -46,7 +65,11 @@ class CrashProofEnvironment(gym.Env):
         return self.env.action_space
 
 
-    def init_environment(self):
+    def init_environment(self) -> None:
+        """Initializes the environment according to the config file given at initializaiton:
+            1) start_process_and_wait_for_startsignal(...)
+            2) get_environment(...)
+        This enviornment is then used for further interaction"""
         self.env_initalizations += 1
         self.logger.info(f"Initializing environment for the {self.env_initalizations}-th time.")
         self.tmi_process, self.control_queue, self.response_queue = start_process_and_wait_for_startsignal(self.cfg, 
@@ -56,6 +79,11 @@ class CrashProofEnvironment(gym.Env):
 
 
     def finalize_process(self, reinit : bool = True):
+        """
+        Sends command to process-wrapper to stop execution.
+        Args:
+            reinit (bool) : If set, it calls self.init_environment() after joining process.
+        """
         self.logger.info("Finalizing tmi-process.")
 
         self.control_queue.put(IPCCommands.get_end_syncloop_command(1000)) #1000 doesnt matter.
@@ -65,18 +93,27 @@ class CrashProofEnvironment(gym.Env):
         if reinit:
             self.queue_empty_error += 1
             self.logger.error(f"Reinitializing environment after queue empty error. Total n errors : {self.queue_empty_error}")
-            self.env.reset()
             self.init_environment()
+            self.env.reset()
 
-
+    def _answer_step_after_crash(self, action) -> tuple[gym.spaces.Dict,float,bool,bool, dict[str, any]]:
+        if self.total_timesteps == 0:
+            self.step(action)
+        return self._last_obs, self._last_rew, self._last_terminated, True, self._last_info
 
     def step(self, action) -> tuple[gym.spaces.Dict,float,bool,bool, dict[str, any]]:
         try:
-            return self.env.step(action)
+            self._last_obs, self._last_rew, self._last_terminated, self._last_truncated, self._last_info = self.env.step(action)
+            self.total_timesteps += 1
+            self._step_recorded_at_timestep = self.total_timesteps
+            return self._last_obs, self._last_rew, self._last_terminated, self._last_truncated, self._last_info
         except TMICommunicationFaildException as rte: # queue-empty error
             traceback.print_exc()
             self.finalize_process()
-            return self.step(action)
+            return self._answer_step_after_crash(action)
+
+
+
 
     def reset(self, seed = None, options = None) -> tuple[gym.spaces.Dict, dict[str, any]]:
         try:
@@ -84,7 +121,7 @@ class CrashProofEnvironment(gym.Env):
         except TMICommunicationFaildException as rte: # queue-empty error
             traceback.print_exc()
             self.finalize_process()
-            return self.reset()
+            return self.reset(seed = seed, options = options)
 
 
     def render(self, mode: str = "human") -> Optional[np.array]:
@@ -93,4 +130,4 @@ class CrashProofEnvironment(gym.Env):
         except TMICommunicationFaildException as rte: # queue-empty error
             traceback.print_exc()
             self.finalize_process()
-            return self.render()
+            return self.render(mode=mode)
