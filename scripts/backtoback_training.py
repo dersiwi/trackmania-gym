19ae8147557d4d3ba9aeffbbb042e36161c6a939
Simon Wilhelm Schübel <siwischbl@gmail.com>
2025-08-21 22:22:50 +0200

    Fixed backtoback trainig script

--- scripts/backtoback_training.py
+++ scripts/backtoback_training.py
@@ -4,15 +4,16 @@ Given a bunch of configurations, run back to back training-scripts, without huma
 
 import os
 
-folder_path = r"C:\Users\siwis\Documents\makecargofast\trackmania-gym\configs\trainings_configs"
+folder_path = os.path.join(os.getcwd(), r"configs\trainings_configs")
+assert os.path.exists(folder_path)
 yaml_files = []
 with open("configs/trainings_configs/listfile.txt", "r") as file:
     for i, line in enumerate(file):
         yaml_files.append(line.strip().strip("\n"))
-        assert os.path.exists(yaml_files[i]), f"Could not find file : '{yaml_files[i]}' "
+        assert os.path.exists(os.path.join(folder_path, yaml_files[i])), f"Could not find file : '{yaml_files[i]}' "
 input(f"Confiform blocktraining on {yaml_files} \n[Press anything to continue]")
 
 
 for config in yaml_files:
-    os.system(f"python scripts/train.py --config-path={folder_path} --config-name={config}")
+    os.system(f"python scripts/sb3_train.py --config-path={folder_path} --config-name={config}")
     #TODO : Make sure game is actually closed.
