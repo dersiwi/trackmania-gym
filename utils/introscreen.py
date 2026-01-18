fd479c5c96c79d4b3c6c272e3b0aef7a3e1c4c42
SgSiegens <uxgzc@student.kit.edu>
2026-01-18 10:20:14 +0100

    further rewritten some reward manager yamls and also changed the way how the reward manager name gets displayed in the intro screen

--- utils/introscreen.py
+++ utils/introscreen.py
@@ -119,7 +119,7 @@ def introscreen(cfg : TrainConfig, asciiart = welcome, askstart : bool = False):
     print(f"""\n============================= General =============================
             - Observation-Manager   : {cfg.rl_env.obs_manager.name}
               - use images          : {secure_attribute_retrieval(lambda : cfg.rl_env.env.obs_have_imgs)}
-            - Reward-Manager        : {cfg.rl_env.reward_manager.name}
+          - Reward-Manager        : {secure_attribute_retrieval(lambda : cfg.rl_env.reward_manager.name, cfg.rl_env.reward_manager._target_)}
             - Termination-Manager   : {cfg.rl_env.termination_manager.name}
             - RL-scheduler          : {cfg.lr_scheduler._target_}
             - n_env_steps           : {cfg.learn_args.total_timesteps}
