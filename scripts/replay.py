63a7bfd60a386051adc4d540b80a0d08bdda655f
SgSiegens <uxgzc@student.kit.edu>
2026-01-27 09:51:05 +0100

    updated the replay script to work with the new code changes

--- scripts/replay.py
+++ scripts/replay.py
@@ -1,4 +1,3 @@
-
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # TODO : <- i don't want this here and it shouldnt have to be here!!!
 
@@ -8,14 +7,9 @@ from omegaconf import DictConfig, OmegaConf
 from configs.config import TrainConfig
 import traceback
 
-
-
-from game_interaction.run_multiprocess_wrapper import start_process_and_wait_for_startsignal
-from game_interaction.ipc_fields import IPCCommands
-
-from trackmania_env.envs.enivonrments import get_environment
-
-from utils.hydra_wandb_utils import get_models, load_and_merge_platform
+from utils.hydra_wandb_utils import  load_and_merge_platform
+from tmn_sb3.utils.from_cfg import get_model_from_config
+from trackmania_env.envs.sec_env import CrashProofEnvironment
 
 run = None
 run_path_hydra = None
@@ -30,14 +24,14 @@ _HYDRA_PARAMS = {
 
 @hydra.main(**_HYDRA_PARAMS)
 def main(cfg : TrainConfig):
+
     cfg = load_and_merge_platform(cfg)
-    tmi_process, control_queue, response_queue = start_process_and_wait_for_startsignal(cfg, cfg.rl_env.obs_manager.img_width, cfg.rl_env.obs_manager.img_height)
+    tm_env = CrashProofEnvironment(cfg)
 
     try:
-        tm_env = get_environment(cfg, control_queue, response_queue)
+        tm_env.init_environment()
         
-        # get algorithm and start learning process
-        model = get_models(cfg, tm_env, print_params = True,load_model_path= model_path)
+        model = get_model_from_config(cfg = cfg, tm_env = tm_env, print_params= False, run_id= "xyz" ,load_model_path=model_path)
         #model.policy.features_extractor.eval()
         # this should be sufficient 
         eval_policy = model.policy.eval()
@@ -48,6 +42,7 @@ def main(cfg : TrainConfig):
             action, state = eval_policy.predict(observations, deterministic=True)
             observations, reward, terminated, truncated, info = tm_env.step(action)
             if terminated or truncated: tm_env.reset()
+
     except Exception as e:
         traceback.print_exc()
 
@@ -56,8 +51,7 @@ def main(cfg : TrainConfig):
 
     finally:
         # Finalize training and close game all processes.
-        control_queue.put(IPCCommands.get_end_syncloop_command()) 
-        tmi_process.join()
+        tm_env.finalize_process(reinit=False)
         
 
 if __name__ == "__main__":
