2dc6cb0929abb95bff5cad75733864da180eab44
SgSiegens <uxgzc@student.kit.edu>
2026-02-05 13:34:00 +0100

    added the first prototype of the simbav2 actor

--- tmn_sb3/simbav2/simbav2_networks.py
+++ tmn_sb3/simbav2/simbav2_networks.py
@@ -9,6 +9,88 @@ import torch
 import torch.nn as nn
 
 
+def create_simbav2_base(
+    num_blocks: int,
+    in_features: int,
+    hidden_features: int,
+    scaler_init: float,
+    scaler_scale: float,
+    alpha_init: float,
+    alpha_scale: float,
+    c_shift: float,
+    gain: float,
+) -> tuple[nn.Module, nn.Module]:
+    embedder = HyperEmbedder(
+        in_features=in_features,
+        out_features=hidden_features,
+        scaler_init=scaler_init,
+        scaler_scale=scaler_scale,
+        c_shift=c_shift,
+        gain=gain,
+    )
+
+    lerp_blocks = [
+        HyperLERPBlock(
+            in_features=hidden_features,
+            out_features=hidden_features,
+            hidden_features=hidden_features,
+            scaler_init=scaler_init,
+            scaler_scale=scaler_scale,
+            alpha_init=alpha_init,
+            alpha_scale=alpha_scale,
+            gain=gain,
+        )
+        for _ in range(num_blocks)
+    ]
+
+    encoder = nn.Sequential(*lerp_blocks)
+
+    return embedder, encoder
+
+
+class SimbaV2Actor(nn.Module):
+    def __init__(
+        self,
+        num_blocks: int,
+        in_features: int, # TODO: this must be the dimension of the observations
+        hidden_features: int,
+        action_dim: int,
+        scaler_init: float,
+        scaler_scale: float,
+        alpha_init: float,
+        alpha_scale: float,
+        c_shift: float,
+        gain: float = 1.0,
+    ) -> None:
+        super().__init__()
+
+        self.embedder, self.encoder = create_simbav2_base(
+            num_blocks=num_blocks,
+            in_features=in_features,
+            hidden_features=hidden_features,
+            scaler_init=scaler_init,
+            scaler_scale=scaler_scale,
+            alpha_init=alpha_init,
+            alpha_scale=alpha_scale,
+            c_shift=c_shift,
+            gain=gain,
+        )
+
+        self.predictor = HyperNormalTanhPolicy(
+            in_features=hidden_features,
+            hidden_features=hidden_features,
+            action_dim=action_dim,
+            scaler_init=1.0,
+            scaler_scale=1.0,
+        )
+
+    def forward(self, observations: torch.Tensor, temperature: float = 1.0):
+        x = self.embedder(observations)
+        x = self.encoder(x)
+        dist = self.predictor(x, temperature)
+        return dist
+
+
 class SimbaV2Critic(nn.Module):
     def __init__(
         self,
@@ -30,30 +112,18 @@ class SimbaV2Critic(nn.Module):
         # TODO: in_features must be dimension of the action concatenated with the observations at axis 1
         # this means that the obs can not be dicts
         # maybe parse them observation spaces in the constructor and then calculated them manually
-        self.embedder = HyperEmbedder(
+
+        self.embedder, self.encoder = create_simbav2_base(
+            num_blocks=num_blocks,
             in_features=in_features,
-            out_features=hidden_features,
+            hidden_features=hidden_features,
             scaler_init=scaler_init,
             scaler_scale=scaler_scale,
+            alpha_init=alpha_init,
+            alpha_scale=alpha_scale,
             c_shift=c_shift,
             gain=gain,
         )
-
-        lerp_blocks = [
-            HyperLERPBlock(
-                in_features=hidden_features,
-                out_features=hidden_features,
-                hidden_features=hidden_features,
-                scaler_init=scaler_init,
-                scaler_scale=scaler_scale,
-                alpha_init=alpha_init,
-                alpha_scale=alpha_scale,
-                gain=gain,
-            )
-            for _ in range(num_blocks)
-        ]
-
-        self.encoder = nn.Sequential(*lerp_blocks)
 
         self.predictor = HyperCategoricalValue(
             in_features=hidden_features,
