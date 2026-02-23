48b5a142e14cd9b46c92688ce38ef6a0446c2ef5
Simon Wilhelm Schübel <siwischbl@gmail.com>
2026-02-23 18:27:08 +0100

    New rewrards, new config.

--- trackmania_env/rewards/implementations/basic_rewards.py
+++ trackmania_env/rewards/implementations/basic_rewards.py
@@ -5,7 +5,7 @@ from trackmania_env.rewards.reward_calculation import RewradCalculator
 from trackmania_env.rewards.reward_terms.basic_terms import DriveForwardReward
 from game_interaction.ipc_fields import IPCFields
 import numpy as np
-
+from trackmania_env.rewards.reward_terms.basic_terms import AccumulatedDistanceReward
 class ForwardReward(RewradCalculator):
 
     def __init__(self, drive_forward_reward,
@@ -28,3 +28,30 @@ class ForwardReward(RewradCalculator):
     def reset(self):
         self.current_refline_idx = 0
         return super().reset()
+    
+
+class AccumRewards(RewradCalculator):
+    
+    def __init__(self, 
+                 accum_distance_weight: float,
+                accum_enhanced_by_amount_travelled: bool,
+                accum_exp_factor: float,
+                normalize : bool = False,
+                **kwargs ):
+        """
+        Initializes the reward manager with explicit reward weights and parameters
+        for modeling centerline distance reward using a Gaussian function.
+
+        Args:
+            drive_forward_reward (float):   Weight for forward-acceleration acitons
+    """
+        
+        super().__init__(normalize)
+
+        self.terms = [AccumulatedDistanceReward(
+                accum_distance_weight,
+                enhanced_by_amount_travelled=accum_enhanced_by_amount_travelled,
+                exponential_factor=accum_exp_factor,
+            ),]
+        if len(kwargs) > 0:
+            print(f"Got additional kwargsuments that are not used; ignoring them : {kwargs.keys()}. Maybe they're used by a class that inherits.")
