64f72a203b62ef0502009dac1b3b7b3f46d6273a
SgSiegens <uxgzc@student.kit.edu>
2026-02-28 17:27:22 +0100

    eval seetings can now be specified via the config and also changed the Evalcallback in the SB3 manager

--- utils/experiment_managers/sb3_exp_manager.py
+++ utils/experiment_managers/sb3_exp_manager.py
@@ -1,17 +1,13 @@
 import os
 from typing import List
-from stable_baselines3.common.callbacks import (
-    EventCallback, 
-    CallbackList, 
-    EvalCallback, 
-    CheckpointCallback
-)
+from stable_baselines3.common.callbacks import EventCallback, CallbackList, EvalCallback, CheckpointCallback
 
 from stable_baselines3.common.base_class import BaseAlgorithm
 from gymnasium import Env
-from utils.experiment_managers.core import ExperimentManager 
+from utils.experiment_managers.core import ExperimentManager
 from configs.config import TrainConfig
 from trackmania_env.callbacks import *
+from eval.eval_sb3_callback import TMNFEvalCallback
 
 from omegaconf import OmegaConf
 
@@ -19,30 +15,41 @@ from omegaconf import OmegaConf
 class Sb3ExperimentManager(ExperimentManager):
     """
     A SB3 Manager that automatically configures and registers:
-    Callbacks: 
+    Callbacks:
         1. EvalCallback (for best model)
         2. CheckpointCallback (for periodic checkpoints)
     Artifacts:
         1. best-model
         2. checkpoint-model
     """
-    
-    def __init__(self, hydra_run_dir : str, cfg : TrainConfig, env : Env, eval_freq : int, checkpoint_freq : int, run_id = None, resume = None, n_envs : int = 1):
-        
+
+    def __init__(
+        self,
+        hydra_run_dir: str,
+        cfg: TrainConfig,
+        env: Env,
+        eval_freq: int,
+        checkpoint_freq: int,
+        run_id=None,
+        resume=None,
+        n_envs: int = 1,
+    ):
         super().__init__(hydra_run_dir, cfg, run_id, resume)
-        self.callbacks : List[EventCallback] = []
+        self.callbacks: List[EventCallback] = []
         model_dir = os.path.join(self.hydra_run_dir, "models")
         self.best_model_path = self._setup_path(model_dir, "best_model")
         self.checkpoint_path = self._setup_path(model_dir, "checkpoints")
         self.eval_log_path = self._setup_path(self.hydra_run_dir, "eval_logs")
-        
+
         self.eval_freq = eval_freq
         self.checkpoint_freq = checkpoint_freq
         self.n_envs = n_envs
-       
+
         self.append_callbacks(env)
         self.add_artifacts()
-        
+
+        self.cfg = cfg
+        self.cfg.gmi.port = self.cfg.gmi.port + 20  # to prevent port clashes
 
     def get_callbacks(self) -> CallbackList:
         """
@@ -53,52 +60,61 @@ class Sb3ExperimentManager(ExperimentManager):
     def append_callbacks(self, env):
         """Append Evaluation-callback and Checkpoint-Callback"""
         # Eval Callback saves best model
-        eval_callback = EvalCallback(
-            env, 
+        eval_callback = TMNFEvalCallback(
+            cfg=self.cfg,
+            n_eval_episodes=self.cfg.eval.n_eval_episodes,
+            eval_freq=self.cfg.eval.eval_freq,
             best_model_save_path=self.best_model_path,
             log_path=os.path.join(self.hydra_run_dir, "eval_logs"),
-            eval_freq= self.eval_freq, 
-            deterministic=True,
-            render=False,
+            deterministic=self.cfg.deterministic,
+            render= self.cfg.eval.render,
+            render_mode= self.cfg.eval.render_mode,
+            verbose= self.cfg.verbose,
+            warn=self.eval.warn,
+            save_vecnormalize= self.cfg.eval.save_vecnormalize,
         )
-        self.callbacks.append(eval_callback)  
+        # eval_callback = EvalCallback(
+        #     env,
+        #     best_model_save_path=self.best_model_path,
+        #     log_path=os.path.join(self.hydra_run_dir, "eval_logs"),
+        #     eval_freq=self.eval_freq,
+        #     deterministic=True,
+        #     render=False,
+        # )
+        self.callbacks.append(eval_callback)
 
         # Checkpoint Callback save model every N steps
         checkpoint_callback = CheckpointCallback(
-            save_freq= self.checkpoint_freq,
+            save_freq=self.checkpoint_freq,
             save_path=self.checkpoint_path,
             name_prefix="checkpoint",
             save_replay_buffer=False,
-            save_vecnormalize=False,
+            save_vecnormalize= self.cfg.eval.save_vecnormalize
         )
         self.callbacks.append(checkpoint_callback)
-        
+
         if self.use_wandb:
             self.callbacks.append(AccumRewardLogCallback(n_envs=self.n_envs))
             self.callbacks.append(ReturnCallback())
             self.callbacks.append(FurtherStatisticsCallback())
-            
+
             if self.cfg.rl_env.env.continuous_actions:
                 self.callbacks.append(ContinuousActionLogCallback(log_minmax=self.cfg.wandb.logminmax_continuous))
 
-        if self.cfg.debug and OmegaConf.select(cfg=self.cfg,key="rl_env.obs_manager.obs_have_imgs",default=False):
-            self.callbacks.append(ImageDumpCallback(verbose=0,dump_freq=self.cfg.img_dump_freq,dump_dir=self.cfg.img_dump_path))
+        if self.cfg.debug and OmegaConf.select(cfg=self.cfg, key="rl_env.obs_manager.obs_have_imgs", default=False):
+            self.callbacks.append(
+                ImageDumpCallback(verbose=0, dump_freq=self.cfg.img_dump_freq, dump_dir=self.cfg.img_dump_path)
+            )
 
     def add_artifacts(self):
         """Add best model and checkpoint-model artifact"""
-        best_model_zip_path = os.path.join(self.best_model_path, "best_model.zip") # Register the best_model.zip file that EvalCallback will create
-        self.add_artifact(
-            artifact_name="best_model",
-            artifact_type="model",
-            path=best_model_zip_path
-        )
+        best_model_zip_path = os.path.join(
+            self.best_model_path, "best_model.zip"
+        )  # Register the best_model.zip file that EvalCallback will create
+        self.add_artifact(artifact_name="best_model", artifact_type="model", path=best_model_zip_path)
 
         # Register all checkpoint files using a glob pattern
-        self.add_artifact(
-            artifact_name="checkpoint_model",
-            artifact_type="model",
-            path=self.checkpoint_path
-        )
+        self.add_artifact(artifact_name="checkpoint_model", artifact_type="model", path=self.checkpoint_path)
 
     def _setup_path(self, base_dir: str, sub_dir: str) -> str:
         path = os.path.join(base_dir, sub_dir)
@@ -108,15 +124,11 @@ class Sb3ExperimentManager(ExperimentManager):
     def _additional_post_processing(self, model: BaseAlgorithm):
         final_model_path = os.path.join(self.hydra_run_dir, "model.zip")
         model_save_base = os.path.join(self.hydra_run_dir, "model")
-        
+
         try:
             print(f"Saving final model to {model_save_base}...")
-            model.save(model_save_base) 
-            
-            self.add_artifact(
-                artifact_name="final_model",
-                artifact_type="model",
-                path=final_model_path
-            )
+            model.save(model_save_base)
+
+            self.add_artifact(artifact_name="final_model", artifact_type="model", path=final_model_path)
         except Exception as e:
             print(f"Error saving final model: {e}")
